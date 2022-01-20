d8d5d9eba9d1d8158ff9ba925299ce714cfc963e
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-20 14:14:26 -0600

    ....

---
+++ src/app/Task.ts
@@ -0,0 +1,7 @@
+export interface Task {
+    id?: number;
+    text: String;
+    day: String;
+    isReminder: boolean;
+
+}
--- src/app/mock-task.ts
+++ src/app/mock-task.ts
@@ -1,14 +1,20 @@
 export const TASK = [
-    {id: 1,
-    text: "Doctors Appointment", 
-    day: "May 5th at 2:30pm",
-    isReminder: true},
-    {id: 2,
-    text: "Meeting at School", 
-    day: "May 6th at 1:30pm",
-    isReminder: true},
-    {id: 3,
-    text: "Food Shopping", 
-    day: "May 6th at 12:30pm",
-    isReminder: false}
-]
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    isReminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'May 6th at 1:30pm',
+    isReminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'May 6th at 12:30pm',
+    isReminder: false,
+  },
+];
